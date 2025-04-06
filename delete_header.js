60116884209eaaf0956e1e6f01f6820d5f186fee
Quang Huan <nquanghuan0806@gmail.com>
2025-04-06 15:26:29 +0700

    Update delete_header.js

--- delete_header.js
+++ delete_header.js
@@ -1,6 +1,5 @@
-const deleteInvalidHeader = (request) => {
-    request.headers = request.headers.filter(header => header !== 'Authorization');
-    return request;
-};
 
-$httpClient.request = deleteInvalidHeader($httpClient.request);
+const version = 'V1.0.2';
+
+
+function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
